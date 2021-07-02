5b46cf18bb91d1c3b2429a4c29d31c3b69fb01bd
Marcos Lopes <marcoslopesg7@gmail.com>
2021-07-02 10:47:25 -0300

    Axios autenticação

--- src/views/admin/Cidades.vue
+++ src/views/admin/Cidades.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="cadastro-cidade">
-
     <v-dialog v-model="dialog" max-width="600px">
       <v-card>
         <v-card-title>
@@ -10,15 +9,17 @@
 
         <v-form ref="form" class="pa-8 mx-auto">
           <v-text-field
-            v-model="cidade.nome_cidade"
+            v-model="cidade.nome"
             label="Nome:"
             required
+            :error-messages="error.nome"
           ></v-text-field>
 
           <v-text-field
             v-model="cidade.uf"
             label="UF:"
             required
+            :error-messages="error.uf"
           ></v-text-field>
 
           <v-btn color="error" class="mr-4 mt-6" @click="modal">
@@ -30,6 +31,7 @@
             outlined
             class="mr-4 mt-6"
             @click="cadastro_cidade"
+            :loading="carregandoSave"
           >
             <v-icon dark> mdi-check </v-icon>
             Salvar
@@ -39,11 +41,9 @@
     </v-dialog>
     <v-row align="center" justify="space-between" class="linha-top">
       <h2>Cadastro de cidades</h2>
-      
+
       <v-btn tile class="button-cadastro" color="#00a65a" @click="modal">
-        <v-icon left>
-          mdi-pencil
-        </v-icon>
+        <v-icon left> mdi-pencil </v-icon>
 
         Cadastrar cidade
       </v-btn>
@@ -56,114 +56,122 @@
     >
     </v-data-table>
   </div>
-
-  
 </template>
 
 <script>
-import axios from "../../../axios/service_public.js";
+import axios from "../../../axios/service.js";
 
 export default {
-  name: 'cidades',
+  name: "cidades",
 
-  data: ()=>{
-
-    return{
+  data: () => {
+    return {
       cidades: [],
       dialog: false,
-      cidade:{
-        nome_cidade: '',
-        uf: ''
+      carregandoSave: false,
+      cidade: {
+        nome: "",
+        uf: "",
+      },
+      error: {
+        nome: "",
+        uf: "",
       },
       headers: [
         {
-          text: 'ID',
-          align: 'start',
+          text: "ID",
+          align: "start",
           sortable: false,
-          value: 'id',
+          value: "id",
         },
-        { text: 'Nome', value: 'nome' },
-        { text: 'UF', value: 'uf' },
-        { text: 'criado em', value: 'created_at' },
+        { text: "Nome", value: "nome" },
+        { text: "UF", value: "uf" },
+        { text: "criado em", value: "created_at" },
       ],
-
-    }
+    };
   },
 
-  methods:{
-
-    modal(){
+  methods: {
+    clear() {
+      this.error = {};
+      this.cidades = {};
+    },
+    modal() {
+      this.clear();
       this.dialog = !this.dialog;
     },
 
-    carregar_cidades(){
-      axios.get('/api/v1/cidades',{
-
-          headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('token_uems')
-          }
-        }).then(response => {
-          if(response.data.status){
-
+    carregar_cidades() {
+      axios
+        .get("/api/v1/cidades")
+        .then((response) => {
+          if (response.data.status) {
             this.cidades = response.data.cidades.data;
-          };
+          }
         })
 
         .catch(function (error) {
-          console.log('[ERRO NA BUSCA DAS CIDADES]: ' + error);
+          console.log("[ERRO NA BUSCA DAS CIDADES]: " + error);
         });
     },
 
-    cadastro_cidade(){
-      axios.post("/api/v1/nova-cidade", {
-        headers: {
-          'Authorization': 'Bearer ' + localStorage.getItem('token_uems')
-        },
-        nome: this.cidade.nome_cidade,
-        uf: this.cidade.uf
-      }).
-      
-      then(response =>{
-        if(response.data.status){
-          this.$toast.open({
-            message: response.data.message,
-            type: "sucess",
-          });
+    cadastro_cidade() {
+      axios
+        .post("/api/v1/nova-cidade", {
+          nome: this.cidade.nome,
+          uf: this.cidade.uf,
+        })
+        .then((response) => {
+          if (response.data.status) {
+            this.$toast.open({
+              message: response.data.message,
+              type: "success",
+            });
 
-          this.carregar_cidades();
-        }
-      })
+            this.carregandoSave = false;
+            this.dialog = !this.dialog;
+            this.carregar_cidades();
+            this.clear();
+          }
+        })
+        .catch((res) => {
+          if (res.response.data && res.response.data.validation) {
+            this.error = res.response.data.validation;
 
-      .catch(()=>{
-        this.$toast.open({
-          message: "Não foi possivel acessar o sistema, acesso negado!",
-          type: "error",
+            this.$toast.open({
+              message: res.response.data.message,
+              type: "error",
+            });
+          } else {
+            this.$toast.open({
+              message: res.response.data.message,
+              type: "error",
+            });
+          }
+          this.carregandoSave = false;
         });
-      })
-    }
+    },
   },
 
-  mounted(){
+  mounted() {
     this.carregar_cidades();
-  }
-}
+  },
+};
 </script>
 
 
 <style>
+.cadastro-cidade h2 {
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
 
-  .cadastro-cidade h2{
-    margin-top: 10px;
-    margin-bottom: 20px;
-  }
+.linha-top {
+  width: 100%;
+  margin: 0 auto;
+}
 
-  .linha-top{
-    width: 100%;
-    margin: 0 auto;
-  }
-
-  .button-cadastro{
-    color: rgba(255,255,255,0.9)!important;
-  }
-
+.button-cadastro {
+  color: rgba(255, 255, 255, 0.9) !important;
+}
 </style>
